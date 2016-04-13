a3ec8be43c8225943de1a12b9d0f8c7fa80a0523
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-04-13 15:57:15 -0500

    ...

--- app/app.js
+++ app/app.js
@@ -19,10 +19,51 @@ class Weather {
       .then((response) => {
         console.log(response);
         let container = document.querySelector("container");
+        let temp = response.list[0].temp.day;
 
         document.querySelector("#city").innerHTML = response.city.name;
         document.querySelector("#condition").innerHTML = response.list[0].weather[0].description;
+        document.querySelector("#current-temp").innerHTML = Math.round(temp) + "&deg;";
+        document.querySelector("#forecast").innerHTML = response.list.forEach((day) => {
+          let date = new Date(day.dt * 1000);
+          let eachDay = date.getDay();
+          let icon = `http://openweathermap.org/img/w/day.weather[0].icon.png`;
+
+     switch (eachDay) {
+       case 0:
+         eachDay = "Monday";
+         break;
+       case 1:
+         eachDay = "Tuesday";
+         break;
+       case 2:
+         eachDay = "Wednesday";
+         break;
+       case 3:
+         eachDay = "Thursday";
+         break;
+       case 4:
+         eachDay = "Friday";
+         break;
+       case 5:
+         eachDay = "Saturday";
+         break;
+       case 6:
+         eachDay = "Sunday";
+         break;
+       default:
+         eachDay = "Error";
+         break;
+     }
+
+        })
+
+
 
       })
+
+
   }
 }
+
+let weather = new Weather();
